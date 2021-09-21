a3c3eedffeafa7a4aaf118d96f8176b02e81f95e
Clara Chu <claradnchu@gmail.com>
2021-09-21 23:53:17 +0930

    Home page feature

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+//import HeaderBar from "./components/HeaderBar";
+import HomeFeature from "./components/HomeFeature";
+
+import {Link } from "react-router-dom";
 
 function Home(){
 	return(
 		<div>
-            <h1>home</h1>
+		<HomeFeature />
 		</div>
-	);
+	)
 }
 export default Home;
