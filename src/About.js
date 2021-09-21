297b2192480dff524e510633043573c2e61688d1
Clara Chu <claradnchu@gmail.com>
2021-09-22 00:07:12 +0930

    About page text and feature added

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components";
+import bg from "./assets/bg.jpg";
+import AboutFeature from "./components/AboutFeature";
+//import HeaderBar from "./components/HeaderBar";
+
+
+function About(){
+	return(
+		<div>
+
+		<AboutFeature />
+		</div>
+	)
+}
+
+export default About;
